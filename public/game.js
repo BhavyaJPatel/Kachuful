e32162e8ee628189d49e760a8a0d7c25f452a3dd
BhavyaJPatel <118665754+BhavyaJPatel@users.noreply.github.com>
2023-05-04 09:31:23 +0530

    Update game.js

--- public/game.js
+++ public/game.js
@@ -1,5 +1,5 @@
 // connecting to the socket server
-const socket = io("http://kachuful.azurewebsites.net");
+const socket = io("https://kachuful.onrender.com/");
 
 
 // on connection this code will be executed
